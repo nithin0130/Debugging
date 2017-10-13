10f42609053f4f2f79c33961444c204a83dab5ba
nithin0130 <30785672+nithin0130@users.noreply.github.com>
2017-10-13 21:27:18 +1100

    Dice.java ( getValue() for random values
    Upd

---
+++ Dice.java
@@ -0,0 +1,21 @@
+public class Dice {
+			
+	private DiceValue value;
+	
+	public Dice() {
+		value =  DiceValue.getRandom();
+	}
+	
+	public DiceValue getValue() {
+	value =  DiceValue.getRandom();	
+            return value;
+	}
+
+	public DiceValue roll() {
+		return DiceValue.getRandom();
+	}		
+	
+	public String toString() {
+		return value.toString();
+	}
+}
